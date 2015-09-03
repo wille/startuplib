fc55d096310658c04b58222ccc5e39b823ecc27b
redpois0n <red@cmail.nu>
2015-09-03 18:48:17 +0200

    Comments and methods renamed

--- src/startuplib/Startup.java
+++ src/startuplib/Startup.java
@@ -1,11 +1,50 @@
 package startuplib;
 
+import java.io.File;
+
 import com.redpois0n.oslib.AbstractOperatingSystem;
 import com.redpois0n.oslib.OperatingSystem;
 
 public class Startup {
+	
+	/**
+	 * Adds current JAR to startup with filename as name
+	 * @throws Exception
+	 */
+	public static void add() throws Exception {	
+		File file = Utils.getJarFile();
 
-	public static void install(String name, String path) throws Exception {
+		add(file.getName());
+	}
+	
+	/**
+	 * Adds current JAR to startup
+	 * @param name Name of key/file
+	 * @throws Exception
+	 */
+	public static void add(String name) throws Exception {
+		File file = Utils.getJarFile();
+		
+		add(name, file.getAbsolutePath());
+	}
+
+	/**
+	 * Add file to startup
+	 * @param name Name of key/file
+	 * @param file File to add
+	 * @throws Exception
+	 */
+	public static void add(String name, File file) throws Exception {
+		add(name, file.getAbsolutePath());
+	}
+	
+	/**
+	 * Add file to startup
+	 * @param name Name of key/file
+	 * @param path Path to file
+	 * @throws Exception
+	 */
+	public static void add(String name, String path) throws Exception {
 		AbstractOperatingSystem os = OperatingSystem.getOperatingSystem();
 		
 		AbstractStartup startup;
